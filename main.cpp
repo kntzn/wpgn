596e717dee1330107e61b0f1d6115a76dc8411d2
kntzn <kntznart@gmail.com>
2020-07-15 21:54:22 +0300

    [Imp]: Code cleanup

--- main.cpp
+++ main.cpp
@@ -11,21 +11,26 @@
 
 #define UNIX_PERIOD_DURATION_MS ((float)(60 * 1000) / UPM)
 
+#define TAU (2*M_PI)
+
 #define ver "1.1.1"
 // ------------------- LOG -------------------
 // [1.1.0]: (add) Keyboard interrupt handler
 // [1.1.1]: (add) Cross-platform delay
-
+// [1.1.2]: (imp) Code cleanup
 
 #include <SFML/Graphics.hpp>
-#include <ctime>
-#include <cmath>
 #include <iostream>
-#include <stdlib.h>
 #include <string>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
 #include <csignal>
 //#include "ImageGenerator.h"
 
+
+
+// TODO: move to the graphics module
 sf::Color multiplex (sf::Color c1, sf::Color c2)
     {
     sf::Color result;
@@ -36,12 +41,12 @@ sf::Color multiplex (sf::Color c1, sf::Color c2)
     return result;
     }
 
-sf::Image makeCopyRed (const int sunSize, sf::Image target, float bright)
+sf::Image makeCopyRed (const int sunSize, const sf::Image& target, float bright)
     {
     // Generates color from height of sun / moon
     sf::Color obj_color;
     obj_color.r = sf::Uint8 (255);
-    obj_color.g = obj_color.b = sf::Uint8 (127) + sf::Uint8 (128 * sqrt (bright));
+    obj_color.g = obj_color.b = sf::Uint8 (127) + sf::Uint8 (128 * std::sqrt (bright));
     
     // Copy of target
     sf::Image sun_cpy = target;
@@ -54,11 +59,12 @@ sf::Image makeCopyRed (const int sunSize, sf::Image target, float bright)
     return sun_cpy;
     }
 
+// TODO: move to the timing module
 // Current time in minutes since 0:00
 float getCurrentTime ()
     {
     // Gets current pc time
-    time_t now = time (0);
+    time_t now = time (nullptr);
     tm* Time = localtime (&now);
     
     return float (Time->tm_hour) * 60.f +
@@ -75,15 +81,18 @@ float getCurrentLocalTime ()
 int getAstronomicalYearDate ()
     {
     // Gets current pc time
-    time_t now = time (0);
+    time_t now = time (nullptr);
     tm* Time = localtime (&now);
     
     return Time->tm_yday + YEARDATE_OFFSET/2;
     }
 
+// TODO: move to the graphics module
 float getLineHeight (float sinAmpl)
     {
-    return float (-sinAmpl * cos (float (getAstronomicalYearDate ())*(2.f*3.14159f) / 365) * fabs (EARTH_AXIS / (90.f - LAT)));
+    return float (-sinAmpl *
+                  cos (float (getAstronomicalYearDate ())*TAU / 365) *
+                  fabs (EARTH_AXIS / (90.f - LAT)));
     }
 
 // Keyboard interrupt handler
@@ -100,6 +109,7 @@ int main()
     std::cout << "WPGN (Wallpaper generator)\n"
                  "Ver. " << ver << std::endl;
     
+    // TODO: get window size?
     // ParamEters
     const int sizeX = 1920;
     const int sizeY = 1080;
@@ -111,19 +121,23 @@ int main()
     
     const int sunSize = 25;
     
+    // TODO: Cross-platform style (original/pwd)
     // Gets the current dir
     // I'm sure that the much easier way to do this exists
     std::string thisDirectory = "///home/kntzn/Desktop/wp/";
     
+    
+    
+    // TODO: move to the graphics module
     // Loads images
     sf::Image sun;
-    if (sun.loadFromFile (std::string (thisDirectory + "sun.png").c_str ()))
+    if (sun.loadFromFile (std::string (thisDirectory + "sun.png")))
         std::cout << "Loaded sun.png\n";
     else
         std::cout << "Failed to load sun.png\n";
     
     sf::Image moon;
-    if (moon.loadFromFile (std::string (thisDirectory + "moon.png").c_str ()))
+    if (moon.loadFromFile (std::string (thisDirectory + "moon.png")))
         std::cout << "Loaded moon.png\n\n";
     else
         std::cout << "Failed to load moon.png\n\n";
@@ -156,7 +170,7 @@ int main()
         
         // Coords of sun/moon
         float x = float (sinLen)*(currentLocalTimeByMod / float (MIN_PER_DAY));
-        float y = sinAmpl * cos (float ((x) * 2 * 3.14159f) / float (sinLen));
+        float y = sinAmpl * std::cos (float ((x) * TAU) / float (sinLen));
         
         // Zero-angle line height
         float h = getLineHeight (sinAmpl);
@@ -165,25 +179,20 @@ int main()
         bool isDay = (y < h);
         
         // brightness factor
-        float sun_bright = 0;
+        float sun_bright;
         if (isDay)
-            sun_bright = (h - y) / float (h + sizeY / 8);
+            sun_bright = (h - y) / (h + sinAmpl);
         else
-            sun_bright = (y - h) / float (sizeY / 8 - h);
-        
+            sun_bright = (y - h) / (sinAmpl - h);
         
         // moon offset from sun position
         float moon_x_offset = (float (getAstronomicalYearDate ()) + 11 + 356.f) / 29.3f * float (sinLen);
         while (moon_x_offset > sinLen / 2.f)
             moon_x_offset -= sinLen;
         
-        
         // Moon
-        float moon_x = x + moon_x_offset;
-        
-        float moon_y = 200;
-        
-        
+        //float moon_x = x + moon_x_offset;
+        //float moon_y = 200;
         
         // -------- !CALCULATIONS -------- //
         
@@ -194,11 +203,11 @@ int main()
         // draws a sine wave
         for (int i = 0; i < sizeX; i++)
             {
-            sf::Uint8 bright = 255 - sf::Uint8 (255 * pow (float (abs (sizeX / 2 - (i))) / float (sizeX), 0.08));
+            uint8_t bright =  (uint8_t)255*(1.0 - pow (float (std::abs (0.5f - (float)(i)/sizeX)), 0.08));
             sf::Color col (bright, bright, bright);
             
             // Draws the line
-            generated_img.setPixel (i, centerOffset + h, col);
+            generated_img.setPixel (i, centerOffset + (int)h, col);
             
             // Colors the sine if pc was used
             
@@ -208,26 +217,28 @@ int main()
             // Colors the pixel
             if (i <= sizeX / 2)
                 if (usage [(timeIndexFromIter + int (x) + MIN_PER_DAY) % MIN_PER_DAY])
-                    col = (bright > 63) ? sf::Color (255, 127, 0) : sf::Color (bright * 4, bright * 2, 0);
+                    col = (bright > 63) ?
+                            sf::Color (255, 127, 0) :
+                            sf::Color (bright * 4, bright * 2, 0);
             
             // Draws the sine
-            generated_img.setPixel (i, centerOffset - int (sinAmpl * cos (float ((i + x - xOffset) * 2 * 3.14159f) / float (sinLen))), col);
+            generated_img.setPixel (i,
+                                    centerOffset -
+                                    int (sinAmpl * cos ((((double)(i - xOffset) + x) / sinLen) * TAU)),
+                                    col);
             }
         
         // Draws sun or moon
-        sf::Image* currentImg = nullptr;
+        sf::Image* currentImg;
         currentImg = &sun;
         
         generated_img.copy (makeCopyRed (sunSize, *currentImg, sun_bright),
                             sizeX / 2           - sunSize / 2,
                             sizeY / 2 + int (y) - sunSize / 2);
         
-        
         //if (isDay)
-        
         //else
         //currentImg = &moon;
-        
         //generated_img.copy (moon, moon_x, moon_y);
         
         
@@ -239,11 +250,11 @@ int main()
         usage [int (currentLocalTimeByMod)] = USAGE_VISUALIZATION_MINUTES;
         
         // Reduces old values
-        for (int i = 0; i < MIN_PER_DAY; i++)
-            if (usage [i] > 0)
-                usage [i] -= (MIN_PER_DAY + int (currentLocalTimeByMod) - lastUsageTime) % MIN_PER_DAY;
+        for (int & i : usage)
+            if (i > 0)
+                i -= (MIN_PER_DAY + int (currentLocalTimeByMod) - lastUsageTime) % MIN_PER_DAY;
             else
-                usage [i] = 0;
+                i = 0;
         
         // Updates last usage
         lastUsageTime = int (currentLocalTimeByMod);
@@ -253,15 +264,17 @@ int main()
         // -------- WALLPAPER -------- //
         
         // Saves result
-        if (generated_img.saveToFile (std::string (thisDirectory + "bgr.png").c_str ()))
+        if (generated_img.saveToFile (std::string (thisDirectory + "bgr.png")))
             std::cout << "Saved bgr.png" << std::endl;
         else
             std::cout << "Failed to save bgr.png" << std::endl;
         
         // DO NOT TOUCH
         #ifdef WINDOWS
-        if (SystemParametersInfoA (SPI_SETDESKWALLPAPER, 0, (PVOID)((thisDirectory + "bgr.png").c_str ()), SPIF_UPDATEINIFILE))
-       		    std::cout << "Wallpaper set" << std::endl;
+            if (SystemParametersInfoA (SPI_SETDESKWALLPAPER, 0,
+                                       (PVOID)((thisDirectory + "bgr.png").c_str ()),
+                                       SPIF_UPDATEINIFILE))
+                std::cout << "Wallpaper set" << std::endl;
 	        else
         	    std::cout << "Failed to set wallpaper" << std::endl;
         #else
@@ -270,12 +283,11 @@ int main()
                  thisDirectory + "bgr.png'").c_str ());
         #endif
         
-        clock_t dt = clock () - exec_start;
-        
         // -------- !WALLPAPER -------- //
         
         // -------- DELAY -------- //
-        //
+        clock_t dt = clock () - exec_start;
+    
         #ifdef WINDOWS
             if (dt < 60 * 1000 / UPM)
                 Sleep (60 * 1000 / UPM - dt);
